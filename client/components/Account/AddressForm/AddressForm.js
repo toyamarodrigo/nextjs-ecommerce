e30145ce86892f1758b064a7720ce534954a61f1
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-04-04 05:57:57 -0300

    added address form, permission to strapi

--- client/components/Account/AddressForm/AddressForm.js
+++ client/components/Account/AddressForm/AddressForm.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Form, Button, ButtonOr } from 'semantic-ui-react';
+import { Form, Button } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { toast } from 'react-toastify';
 
 export default function AddressForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: async (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
-    <Form>
+    <Form onSubmit={formik.handleSubmit}>
       <Form.Input
         name="title"
         type="text"
         label="Address Title"
         placeholder="Address Title"
+        onChange={formik.handleChange}
+        value={formik.values.title}
+        error={formik.errors.title}
       />
 
       <Form.Group widths="equal">
@@ -17,22 +31,39 @@ export default function AddressForm() {
           type="text"
           label="Name and Lastname"
           placeholder="Name and Lastname"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          error={formik.errors.name}
         />
         <Form.Input
           name="address"
           type="text"
           label="Address"
           placeholder="Address"
+          onChange={formik.handleChange}
+          value={formik.values.address}
+          error={formik.errors.address}
         />
       </Form.Group>
 
       <Form.Group widths="equal">
-        <Form.Input name="city" type="text" label="City" placeholder="City" />
+        <Form.Input
+          name="city"
+          type="text"
+          label="City"
+          placeholder="City"
+          onChange={formik.handleChange}
+          value={formik.values.city}
+          error={formik.errors.city}
+        />
         <Form.Input
           name="state"
           type="text"
           label="State"
           placeholder="State"
+          onChange={formik.handleChange}
+          value={formik.values.state}
+          error={formik.errors.state}
         />
       </Form.Group>
 
@@ -42,12 +73,18 @@ export default function AddressForm() {
           type="text"
           label="Postal Code"
           placeholder="Postal Code"
+          onChange={formik.handleChange}
+          value={formik.values.postalCode}
+          error={formik.errors.postalCode}
         />
         <Form.Input
           name="phone"
           type="text"
           label="Phone Number"
           placeholder="Phone Number"
+          onChange={formik.handleChange}
+          value={formik.values.phone}
+          error={formik.errors.phone}
         />
       </Form.Group>
       <div className="actions">
@@ -58,3 +95,27 @@ export default function AddressForm() {
     </Form>
   );
 }
+
+const initialValues = () => {
+  return {
+    title: '',
+    name: '',
+    address: '',
+    city: '',
+    state: '',
+    postalCode: '',
+    phone: '',
+  };
+};
+
+const validationSchema = () => {
+  return {
+    title: Yup.string().required(true),
+    name: Yup.string().required(true),
+    address: Yup.string().required(true),
+    city: Yup.string().required(true),
+    state: Yup.string().required(true),
+    postalCode: Yup.string().required(true),
+    phone: Yup.string().required(true),
+  };
+};
