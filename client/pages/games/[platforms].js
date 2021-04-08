bbf2094b7b87d78367258571dd92914713c9b809
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-04-08 02:56:01 -0300

    render games page

--- client/pages/games/[platforms].js
+++ client/pages/games/[platforms].js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Loader } from 'semantic-ui-react';
+import { map, size } from 'lodash';
 import BasicLayout from '../../layouts/BasicLayout';
 import { useRouter } from 'next/router';
+import { getGamesPlatformApi } from '../../api/game';
+import ListGames from '../../components/ListGames/ListGames';
+
+const limitPerPage = 4;
 
 export default function Platform() {
+  const [games, setGames] = useState(null);
   const { query } = useRouter();
+
+  useEffect(() => {
+    (async () => {
+      if (query.platforms) {
+        const response = await getGamesPlatformApi(
+          query.platforms,
+          limitPerPage,
+          0
+        );
+        setGames(response);
+      }
+    })();
+  }, [query]);
 
   return (
     <BasicLayout className="platforms">
-      <h1>Estamos en plataformas: {query.platforms}</h1>
+      {!games && <Loader active>Loading games...</Loader>}
+      {games && size(games) === 0 && (
+        <div>
+          <h3>No games</h3>
+        </div>
+      )}
+      {size(games) > 0 && <ListGames games={games} />}
     </BasicLayout>
   );
 }
