ab4c59e9b5bc5469a8cafab0e941c43847f9ff49
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-04-08 02:37:55 -0300

    ListGame responsive js

--- client/components/ListGames/ListGames.js
+++ client/components/ListGames/ListGames.js
@@ -2,12 +2,36 @@ import React from 'react';
 import { map } from 'lodash';
 import { Image, Grid } from 'semantic-ui-react';
 import Link from 'next/link';
+import useWindowSize from '../../hooks/useWindowSize';
+import {
+  breakpointUpSm,
+  breakpointUpMd,
+  breakpointUpLg,
+  breakpointUpXl,
+} from '../../utils/breakpoints';
 
 export default function ListGames({ games }) {
+  const { width } = useWindowSize();
+
+  const getColumnsRender = () => {
+    switch (true) {
+      case width > breakpointUpXl:
+        return 6;
+      case width > breakpointUpLg:
+        return 5;
+      case width > breakpointUpMd:
+        return 3;
+      case width > breakpointUpSm:
+        return 2;
+      default:
+        return 1;
+    }
+  };
+
   return (
     <div className="list-games">
       <Grid>
-        <Grid.Row columns={5}>
+        <Grid.Row columns={getColumnsRender()}>
           {map(games, (game) => (
             <Game game={game} />
           ))}
