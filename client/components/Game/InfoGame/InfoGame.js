e14028a508bcafb61e6f4ea9bdf090410fea681c
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-04-20 03:37:14 -0300

    game release date InfoGame

--- client/components/Game/InfoGame/InfoGame.js
+++ client/components/Game/InfoGame/InfoGame.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactPlayer from 'react-player/lazy';
 import CarouselScreenshots from '../CarouselScreenshots';
+import moment from 'moment';
+import 'moment/locale/es';
 
 export default function InfoGame({ game }) {
   return (
@@ -15,6 +17,13 @@ export default function InfoGame({ game }) {
         title={game.title}
         screenshots={game.screenshots}
       />
+      <div className="info-game__content">
+        <div dangerouslySetInnerHTML={{ __html: game.summary }} />
+        <div className="info-game__content-date">
+          <h4>Fecha de lanzamiento: </h4>
+          <p>{moment(game.releaseDate).format('LL')}</p>
+        </div>
+      </div>
     </div>
   );
 }
