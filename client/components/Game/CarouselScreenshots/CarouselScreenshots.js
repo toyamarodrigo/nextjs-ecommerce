63d31858be130b0e8bfe4d1aebb99da2e2dd8b47
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2021-04-20 03:30:34 -0300

    slider modal open on game images

--- client/components/Game/CarouselScreenshots/CarouselScreenshots.js
+++ client/components/Game/CarouselScreenshots/CarouselScreenshots.js
@@ -1,24 +1,41 @@
-import React from 'react'
-import { Image } from 'semantic-ui-react'
-import Slider from 'react-slick'
-import {map} from 'lodash'
+import React, { useState } from 'react';
+import { Image, Modal } from 'semantic-ui-react';
+import Slider from 'react-slick';
+import { map } from 'lodash';
 
 const settings = {
-  className: "carousel-screenshots",
+  className: 'carousel-screenshots',
   dots: false,
   infinite: true,
   speed: 500,
   slidesToShow: 3,
   swipeToSlider: true,
-   
-}
+};
 
-export default function CarouselScreenshots({game, title, screenshots}) {
+export default function CarouselScreenshots({ game, title, screenshots }) {
+  const [showModal, setShowModal] = useState(false);
+  const [urlImage, setUrlImage] = useState(null);
+
+  const openImage = (url) => {
+    setUrlImage(url);
+    setShowModal(true);
+  };
+
   return (
-    <Slider {...settings}>
-      {map(screenshots, (screenshot) => (
-        <Image key={screenshot.id} src={screenshot.url} alt={screenshot.name} onClick={() => console.log("Abrir Imagen")} />
-      ))}
-    </Slider>
-  )
+    <>
+      <Slider {...settings}>
+        {map(screenshots, (screenshot) => (
+          <Image
+            key={screenshot.id}
+            src={screenshot.url}
+            alt={screenshot.name}
+            onClick={() => openImage(screenshot.url)}
+          />
+        ))}
+      </Slider>
+      <Modal open={showModal} onClose={() => setShowModal(false)} size="large">
+        <Image src={urlImage} alt={title} />
+      </Modal>
+    </>
+  );
 }
